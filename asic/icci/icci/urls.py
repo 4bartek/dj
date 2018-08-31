b6f0c77c42f3c213672939f98b3816cfde122ba1
Bartłomiej Formella <aLive@Mac-mini-Bartomiej.local>
2018-08-31 15:29:50 +0200

    80

--- asic/icci/icci/urls.py
+++ asic/icci/icci/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('crypto/', include('home.urls')),
     path('video/', include('video.urls')),
     path('rss_reader/', include('rss_reader.urls')),
     path('ico/', include('ico.urls')),
